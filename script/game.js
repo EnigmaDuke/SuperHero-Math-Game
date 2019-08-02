5dc1cff1acda37c0b43b555ac5aba7ce37b6a88b
Joshua Duke <dukejoshuae@gmail.com>
2019-08-02 00:47:30 -0400

    Added some game logic

--- script/game.js
+++ script/game.js
@@ -5,8 +5,10 @@ let hitTracker = 0;
 let miss = document.querySelector("#miss");
 let missTracker = 0;
 let score = document.querySelector("#score");
+let scoreTracker = 0;
 let cursor = document.querySelector("#cursor");
 let i;
+
 //Generate Random number
 function getRandomNum(num){
   let randomNum = Math.floor(Math.random() * num);
@@ -17,17 +19,35 @@ function getRandomNum(num){
 //randomize starting position of each image object
 window.onload = function(){
   randomPosition();
-  timer();
+  displayResults();
+  // timer();
   cursor.style.display = "block";
 }
 
 //Generate Random Position
 function randomPosition(){
+
   for (i = 0; i < imgContainer.length; i++){
     imgContainer[i].style.top = getRandomNum(374) + "px";
     imgContainer[i].style.left = getRandomNum(1150) + "px";
+    imgContainer[i].style.display = "block";
   }
 }
+
+let timerSpan = document.querySelector("#timer");
+
+//90 second timer
+// function timer(){
+//   let timeLeft = 90;
+//   let countdown = setInterval(()=>{
+//     timerSpan.innerHTML = timeLeft;
+//     timeLeft -= 1;
+//     if (timeLeft <= 0){
+//       clearInterval(countdown);
+//       timer.innerHTML = gameOver();
+//     }
+//   }, 1000);
+// }
 
 //Display Random number on images
 let generatedNum = document.querySelectorAll(".generatedNum");
@@ -42,7 +62,7 @@ function addNumbers(){
   return result;
 }
 let result = addNumbers();
-
+console.log(result);
 function displayResults(){
 //loop through array of images
 
@@ -52,10 +72,9 @@ function displayResults(){
   //choose a random element from array
     generatedNum[i].innerHTML = number;
     // generatedNum[i].setAttribute("value", number);
-    console.log(number);
+    // console.log(number);
     // generatedNum[i].innerHTML = getRandomNum(25); 
   }
-
   //assign the result to an image
   let arrayPosition = getRandomNum(generatedNum.length-1);
   generatedNum[arrayPosition].innerHTML = result;
@@ -63,31 +82,25 @@ function displayResults(){
   console.log(typeof parseInt(generatedNum[0].innerText));
   return result;
 }
-displayResults();
 
 function findResult(){
-  //if result is = to innerHTML
-  // for(i = 0; i < imgContainer.length; i++){
-  //   if(result == this.innerText){
-  //     hitTracker++;
-  //     console.log(hitTracker);
-  //     hits.innerHTML = hitTracker;
-  //   } else {
-  //     this.style.display = "none";
-  //   }
-  // }
   if(result == this.innerText){
-        hitTracker++;
-        console.log(hitTracker);
-        hits.innerHTML = hitTracker;
-      } else {
-        missTracker++;
-        this.style.display = "none";
-        miss.innerHTML = missTracker;
+    scoreTracker += 3;
+    score.innerHTML = scoreTracker;
+    hitTracker++;
+    console.log(hitTracker);
+    hits.innerHTML = hitTracker;
+    reload();
+  } else {
+    scoreTracker--;
+    score.innerHTML = scoreTracker;
+    missTracker++;
+    this.style.display = "none";
+    miss.innerHTML = missTracker;
 
-        if(missTracker == 3){
-          //when game over display a modal to ask to retry or go to the main menu
-          alert("Game Over!!");
+    if(missTracker == 3){
+      //when game over display a modal to ask to retry or go to the main menu
+      gameOver();
         }
       }
 }
@@ -96,22 +109,10 @@ for(i = 0; i < imgContainer.length; i++){
   imgContainer[i].addEventListener("click", findResult);
 }
 
-let timerSpan = document.querySelector("#timer");
-
-function timer(){
-  let timeLeft = 90;
-  let countdown = setInterval(()=>{
-    timerSpan.innerHTML = timeLeft;
-    timeLeft -= 1;
-    if (timeLeft <= 0){
-      clearInterval(countdown);
-      timer.innerHTML = gameOver();
-    }
-  }, 1000);
-}
-
 function gameOver(){
   timerSpan.innerHTML = "Game Over";
+  alert("Game Over");
+  reload();
 }
 
 document.addEventListener('mousemove', function(e){
@@ -120,6 +121,12 @@ document.addEventListener('mousemove', function(e){
   cursor.style.left = x + "px";
   cursor.style.top = y + "px";
 })
+
+function reload(){
+  randomPosition();
+  result = addNumbers();
+  displayResults();
+}
 
 //return result 
 //display random num  result on 1 image
