a26d50d1ab5cf35bd727c2e5a01b05777b561e00
Joshua Duke <dukejoshuae@gmail.com>
2019-08-01 22:56:38 -0400

    Added some game logic

--- script/game.js
+++ script/game.js
@@ -1,5 +1,11 @@
 
 let imgContainer = document.querySelectorAll(".imgContainer");
+let hits = document.querySelector("#hits");
+let hitTracker = 0;
+let miss = document.querySelector("#miss");
+let missTracker = 0;
+let score = document.querySelector("#score");
+let cursor = document.querySelector("#cursor");
 let i;
 //Generate Random number
 function getRandomNum(num){
@@ -10,6 +16,13 @@ function getRandomNum(num){
 
 //randomize starting position of each image object
 window.onload = function(){
+  randomPosition();
+  timer();
+  cursor.style.display = "block";
+}
+
+//Generate Random Position
+function randomPosition(){
   for (i = 0; i < imgContainer.length; i++){
     imgContainer[i].style.top = getRandomNum(374) + "px";
     imgContainer[i].style.left = getRandomNum(1150) + "px";
@@ -24,6 +37,7 @@ function addNumbers(){
   let num1 = getRandomNum(25);
   let num2 = getRandomNum(9);
   let result =  num1 + num2;
+  cursor.innerHTML = num1 + " + " + num2;
   console.log(result);
   return result;
 }
@@ -51,26 +65,61 @@ function displayResults(){
 }
 displayResults();
 
-
-
 function findResult(){
-  //if result is = to innerhTML
-  for(i = 0; i < imgContainer.length; i++){
-    if(result == this.innerText){
-      return alert ("hi");
-    } else if (result != parseInt(generatedNum[i].innerText)){
-      this.style.display = "none";
-    }
-  }
-  
-  //ALERT hello
-  
+  //if result is = to innerHTML
+  // for(i = 0; i < imgContainer.length; i++){
+  //   if(result == this.innerText){
+  //     hitTracker++;
+  //     console.log(hitTracker);
+  //     hits.innerHTML = hitTracker;
+  //   } else {
+  //     this.style.display = "none";
+  //   }
+  // }
+  if(result == this.innerText){
+        hitTracker++;
+        console.log(hitTracker);
+        hits.innerHTML = hitTracker;
+      } else {
+        missTracker++;
+        this.style.display = "none";
+        miss.innerHTML = missTracker;
+
+        if(missTracker == 3){
+          //when game over display a modal to ask to retry or go to the main menu
+          alert("Game Over!!");
+        }
+      }
 }
 
 for(i = 0; i < imgContainer.length; i++){
   imgContainer[i].addEventListener("click", findResult);
 }
 
+let timerSpan = document.querySelector("#timer");
+
+function timer(){
+  let timeLeft = 90;
+  let countdown = setInterval(()=>{
+    timerSpan.innerHTML = timeLeft;
+    timeLeft -= 1;
+    if (timeLeft <= 0){
+      clearInterval(countdown);
+      timer.innerHTML = gameOver();
+    }
+  }, 1000);
+}
+
+function gameOver(){
+  timerSpan.innerHTML = "Game Over";
+}
+
+document.addEventListener('mousemove', function(e){
+  let x = e.clientX;
+  let y = e.clientY;
+  cursor.style.left = x + "px";
+  cursor.style.top = y + "px";
+})
 
 //return result 
 //display random num  result on 1 image
