dcbc4ba78f13bc77ff89cca680234f6fbb6b9c90
Joshua Duke <dukejoshuae@gmail.com>
2019-08-11 19:04:28 -0400

    Img containers move independently

--- script/test2.js
+++ script/test2.js
@@ -1,5 +1,7 @@
 let imgContainer = document.querySelectorAll(".imgContainer");
-
+// let arrImgContainer;
+// arrImgContainer = Array.from(imgContainer);
+// console.log(arrImgContainer);
 // document.addEventListener('mousemove', function(e){
 //   let x = e.clientX;
 //   let y = e.clientY;
@@ -12,14 +14,8 @@ let imgContainer = document.querySelectorAll(".imgContainer");
 // });
 
 
-let signs = ["+", "-"];
-let sign;
-function generateSign(){
-sign = signs[Math.floor(Math.random()*signs.length)];
-console.log(typeof "+");
-return sign;
-}
-generateSign();
+
+// generateSign();
 
 function randomPosition()
 {
@@ -30,69 +26,140 @@ function randomPosition()
     imgContainer[i].style.display = "block";
   }
 }
-console.log(sign);
+// console.log(sign);
 // let result = eval(5 + sign + 1);
-let result = eval(`5 ${sign} 1`);
-console.log(result);
+// let result = eval(`5 ${sign} 1`);
+// console.log(result);
 
 
 // start game
-// window.onload =( )=>{
-//   randomPosition();
-//   moveElement();
-// }
+window.onload =( )=>{
+  randomPosition();
+  moveElement();
+}
+
+let signs = ["+", "-"];
+// let sign;
+function generateSign(){
+sign = signs[Math.floor(Math.random()*signs.length)];
+return sign;
+}
 
 let animate;
 
-function moveElement(){
-  function frame(){
-    for (i = 0; i < imgContainer.length; i++)
-    {
-      // imgContainer[i].style.left = parseInt(imgContainer[i].style.left) + eval(`${generateSign} +1`) + "px";
-      // imgContainer[i].style.top = parseInt(imgContainer[i].style.top) + eval(`${generateSign} +1`) + "px";
+//ORIGINAL -- -- -- -- -- -- -- -- -- -- -- -- -- 
+// function moveElement(){
+//   animate = setInterval(frame, 100);
+//   function frame(){
+//     for (i = 0; i < imgContainer.length; i++)
+//     {
+//       // arrImgContainer[i].style.left = parseInt(imgContainer[i].style.left) + eval(`${generateSign()} +1`) + "px";
+//       // arrImgContainer[i].style.top = parseInt(imgContainer[i].style.top) + eval(`${generateSign()} +1`) + "px";
 
-      imgContainer[i].style.left = parseInt(imgContainer[i].style.left) - 1 + "px";
-      imgContainer[i].style.top = parseInt(imgContainer[i].style.top) - 1 + "px";
-      console.log(sign);
-    }
-  }
-  animate = setInterval(frame, 100);
-}
+//       arrImgContainer[i].style.left = parseInt(imgContainer[i].style.left) - 1 + "px";
+//       arrImgContainer[i].style.top = parseInt(imgContainer[i].style.top) - 1 + "px";
+//       console.log(sign);
+//     }
+//   }
+// }
 
-function moveElement(){
-  let xPos = generateSign();
-  let yPos = generateSign();
+// arrImgContainer[1].addEventListener("click", ()=>{
+//   alert("jhello");
+// })
 
-  function frame(){
-    for (i = 0; i < imgContainer.length; i++)
-    {
-      // imgContainer[i].style.left = parseInt(imgContainer[i].style.left) + eval(`${generateSign} +1`) + "px";
-      // imgContainer[i].style.top = parseInt(imgContainer[i].style.top) + eval(`${generateSign} +1`) + "px";
-
-      imgContainer[i].style.left = parseInt(imgContainer[i].style.left) - 1 + "px";
-      imgContainer[i].style.top = parseInt(imgContainer[i].style.top) - 1 + "px";
-      console.log(sign);
-    }
-  }
-  animate = setInterval(frame, 100);
-}
-
-// // setInterval(moveElement, 1000);
-
+// -- -- -- CLOSE -- -- -- 
 
 // function moveElement(){
-//   let pos = randomPosition();
-  
-//   let id = setInterval(frame,10);
+//   let arrImgContainer;
+//   arrImgContainer = Array.from(imgContainer);
+//   animate = setInterval(frame, 100);
 //   function frame(){
-//     // if(pos==350){
-//     //   clearInterval(id);
-//     // } else {
-//       pos++;
-//       for (i = 0; i < imgContainer.length; i++)
-//       {
-//         imgContainer[i].style.top = pos + "px";
-//         imgContainer[i].style.left = pos + "px";
-//       }
-//     }
-// }
+
+    // arrImgContainer[0].style.left = parseInt(arrImgContainer[0].style.left) + eval(`${generateSign()} +1`) + "px";
+    // arrImgContainer[0].style.top = parseInt(arrImgContainer[0].style.top) + eval(`${generateSign()} +1`) + "px";
+    // arrImgContainer[1].style.left = parseInt(arrImgContainer[0].style.left) + eval(`${generateSign()} +1`) + "px";
+    // arrImgContainer[1].style.top = parseInt(arrImgContainer[0].style.top) + eval(`${generateSign()} +1`) + "px";
+    // arrImgContainer[0].style.left = parseInt(arrImgContainer[0].style.left) + eval(`${generateSign()} +1`) + "px";
+    // arrImgContainer[0].style.left = parseInt(arrImgContainer[0].style.left) + eval(`${generateSign()} +1`) + "px";
+    // arrImgContainer[0].style.left = parseInt(arrImgContainer[0].style.left) + eval(`${generateSign()} +1`) + "px";
+    // arrImgContainer[0].style.left = parseInt(arrImgContainer[0].style.left) + eval(`${generateSign()} +1`) + "px";
+    // arrImgContainer[0].style.left = parseInt(arrImgContainer[0].style.left) + eval(`${generateSign()} +1`) + "px";
+    // arrImgContainer[0].style.left = parseInt(arrImgContainer[0].style.left) + eval(`${generateSign()} +1`) + "px";
+    
+    
+      // arrImgContainer[0].style.left = parseInt(arrImgContainer[0].style.left) - 1 + "px";
+      // arrImgContainer[0].style.top = parseInt(arrImgContainer[1].style.top) - 1 + "px";
+      // console.log(sign);
+  //   }
+  // }
+  let FPS = 60;
+
+  function randContainer(){
+    let randContainerNum = Math.floor((Math.random() * 8) + 1);
+     return randContainerNum;
+  }
+
+function moveElement(){
+    let arrImgContainer;
+    // console.log(randContainer());
+  arrImgContainer = Array.from(imgContainer);
+  for (let i = 0; i < imgContainer.length; i++){
+    let exp = Math.floor((Math.random()*4)+1);
+    let num = i;
+    console.log(exp);
+    console.log(num);
+    switch (1){
+      case 1:
+        console.log("case1");
+          let sign1 = generateSign();
+          let sign2 = generateSign();
+          function minusMinus(){
+            arrImgContainer[num].style.left = parseInt(arrImgContainer[num].style.left) + eval(`${sign1} +1`) + "px";
+            arrImgContainer[num].style.top = parseInt(arrImgContainer[num].style.top) + eval(`${sign2} +1`) + "px";
+
+            arrImgContainer[num].style.left = parseInt(arrImgContainer[num].style.left) + eval(`${sign1} +1`) + "px";
+            arrImgContainer[num].style.top = parseInt(arrImgContainer[num].style.top) + eval(`${sign2} +1`) + "px";
+
+            arrImgContainer[num].style.left = parseInt(arrImgContainer[num].style.left) + eval(`${sign1} +1`) + "px";
+            arrImgContainer[num].style.top = parseInt(arrImgContainer[num].style.top) + eval(`${sign2} +1`) + "px";
+
+            arrImgContainer[num].style.left = parseInt(arrImgContainer[num].style.left) + eval(`${sign1} +1`) + "px";
+            arrImgContainer[num].style.top = parseInt(arrImgContainer[num].style.top) + eval(`${sign2} +1`) + "px";
+
+            arrImgContainer[num].style.left = parseInt(arrImgContainer[num].style.left) + eval(`${sign1} +1`) + "px";
+            arrImgContainer[num].style.top = parseInt(arrImgContainer[num].style.top) + eval(`${sign2} +1`) + "px";
+
+            arrImgContainer[num].style.left = parseInt(arrImgContainer[num].style.left) + eval(`${sign1} +1`) + "px";
+            arrImgContainer[num].style.top = parseInt(arrImgContainer[num].style.top) + eval(`${sign2} +1`) + "px";
+
+            arrImgContainer[num].style.left = parseInt(arrImgContainer[num].style.left) + eval(`${sign1} +1`) + "px";
+            arrImgContainer[num].style.top = parseInt(arrImgContainer[num].style.top) + eval(`${sign2} +1`) + "px";
+
+            arrImgContainer[num].style.left = parseInt(arrImgContainer[num].style.left) + eval(`${sign1} +1`) + "px";
+            arrImgContainer[num].style.top = parseInt(arrImgContainer[num].style.top) + eval(`${sign2} +1`) + "px";
+          }
+          console.log(sign1);
+          console.log(sign2);
+          setInterval(minusMinus, 1000 / FPS);
+      break;    
+      // case 2:
+      //     function minusPlus(){
+      //       arrImgContainer[0].style.left = parseInt(arrImgContainer[0].style.left) - 1 + "px";
+      //       arrImgContainer[0].style.top = parseInt(arrImgContainer[0].style.top) + 1 + "px";
+      //     }
+      //     setInterval(minusPlus, 100);
+      // case 3:
+      //     function plusPlus(){
+      //       arrImgContainer[0].style.left = parseInt(arrImgContainer[0].style.left) + 1 + "px";
+      //       arrImgContainer[0].style.top = parseInt(arrImgContainer[0].style.top) + 1 + "px";
+      //     }
+      //     setInterval(plusPlus, 100);
+      // case 4:
+      //     function plusMinus(){
+      //       arrImgContainer[0].style.left = parseInt(arrImgContainer[0].style.left) + 1 + "px";
+      //       arrImgContainer[0].style.top = parseInt(arrImgContainer[0].style.top) - 1 + "px";
+      //     }
+      //     setInterval(plusMinus,100);
+    }
+  }
+}
